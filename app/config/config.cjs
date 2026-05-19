41bc4d5360747e0cf908f40dc279e664d1527556
zerg959 <zerg959@yandex.ru>
2026-05-19 11:25:03 +0300

    fix:bd conn

--- app/config/config.cjs
+++ app/config/config.cjs
@@ -7,11 +7,11 @@ module.exports = {
   },
   production: {
     dialect: 'postgres',
-    database: process.env.DATABASE_NAME,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    port: process.env.DATABASE_PORT,
-    host: process.env.DATABASE_HOST,
+    database: process.env.DATABASE_NAME || 'postgres',
+    username: process.env.DATABASE_USERNAME || 'postgres',
+    password: process.env.DATABASE_PASSWORD || 'postgres',
+    port: process.env.DATABASE_PORT || 'db',
+    host: process.env.DATABASE_HOST || 5432,
   },
   // test: {
   //   dialect: 'sqlite',
@@ -19,10 +19,10 @@ module.exports = {
   // },
   test: {
     dialect: 'postgres',
-    database: process.env.DATABASE_NAME,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    port: process.env.DATABASE_PORT,
-    host: process.env.DATABASE_HOST,
+    database: process.env.DATABASE_NAME || 'postgres',
+    username: process.env.DATABASE_USERNAME || 'postgres',
+    password: process.env.DATABASE_PASSWORD || 'postgres',
+    port: process.env.DATABASE_PORT || 5432,
+    host: process.env.DATABASE_HOST || 'db',
   },
 };
